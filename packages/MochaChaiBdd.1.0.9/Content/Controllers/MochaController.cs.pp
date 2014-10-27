7a6399fdc90cf6e61ecd86b48ead54b1c1b4f2fe
Ben Crinion <ben.crinion@gaist.co.uk>
2014-10-27 09:47:13 +0000

    --amend

---
+++ packages/MochaChaiBdd.1.0.9/Content/Controllers/MochaController.cs.pp
@@ -0,0 +1,14 @@
+﻿#if DEBUG
+namespace $rootnamespace$.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MochaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+#endif
