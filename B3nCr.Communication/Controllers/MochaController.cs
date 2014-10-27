6d21687e2d1df0fff7ce59ec53480d42d2a4a7da
Ben Crinion <ben.crinion@gaist.co.uk>
2014-10-27 09:46:43 +0000

    --amend

---
+++ B3nCr.Communication/Controllers/MochaController.cs
@@ -0,0 +1,14 @@
+#if DEBUG
+namespace B3nCr.Communication.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MochaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+#endif
---
+++ B3nCr.Communication/Scripts/specs/application/calculator.js
@@ -0,0 +1,7 @@
+﻿var expect = chai.expect;
+
+describe('calculator', function() {
+    it('adds', function () {
+        expect(1 + 1).to.equal(2);
+    });
+});
