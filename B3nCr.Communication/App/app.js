5ed8a29222e7c12ac94d430cea79094388546b4c
Ben Crinion <ben.crinion@gaist.co.uk>
2014-11-03 17:36:09 +0000

    removed extra bits of the angularjs app

--- B3nCr.Communication/App/app.js
+++ B3nCr.Communication/App/app.js
@@ -3,9 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.auth',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.auth'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/register'});
